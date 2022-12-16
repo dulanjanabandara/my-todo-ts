f5202ae28b30c66bbfbc4df1e2b8b112ef6c42e0
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-12-16 11:45:37 +0530

    Templates class added and projecy completed.

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,44 @@
+import FullList from "./model/FullList";
+import ListItem from "./model/ListItem";
+import ListTemplate from "./templates/ListTemplates";
+
 import "./css/style.css";
 
+const initApp = (): void => {
+  const fullList = FullList.instance;
+  const template = ListTemplate.instance;
+
+  const itemEntryForm = document.getElementById(
+    "itemEntryForm"
+  ) as HTMLFormElement;
+
+  itemEntryForm.addEventListener("submit", (event: SubmitEvent): void => {
+    event.preventDefault();
+    const input = document.getElementById("newItem") as HTMLInputElement;
+    const newEntryText: string = input.value.trim();
+
+    if (!newEntryText.length) return; // to make sure that we're not adding empty elements to the list. Checking the lingth property of the text value is not needed. An empty string evaluates as false too.
+
+    const itemId: number = fullList.list.length
+      ? parseInt(fullList.list[fullList.list.length - 1].id) + 1
+      : 1;
+
+    const newItem = new ListItem(itemId.toString(), newEntryText);
+    fullList.addItem(newItem);
+    template.render(fullList);
+  });
+
+  const clearItems = document.getElementById(
+    "clearItemsButton"
+  ) as HTMLButtonElement;
+
+  clearItems.addEventListener("click", (): void => {
+    fullList.clearList();
+    template.clear();
+  });
+
+  fullList.load();
+  template.render(fullList);
+};
+
+document.addEventListener("DOMContentLoaded", initApp);
